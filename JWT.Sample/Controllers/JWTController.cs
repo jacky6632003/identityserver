f6d74d22ec05e0e33dd7d6005ad18cf1174ab0c1
jacky6632003 <jacky6632003@gmail.com>
2020-03-12 22:42:13 +0800

    JWT

---
+++ JWT.Sample/Controllers/JWTController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+
+namespace JWT.Sample.Controllers
+{
+    [Route("api/[controller]")]
+    public class JWTController : Controller
+    {
+        private readonly IHttpContextAccessor _accessor;
+
+        public IHttpContextAccessor HttpContextAccessor { get; set; }
+
+        public JWTController(IHttpContextAccessor accessor)
+        {
+            _accessor = accessor;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            // 簡單創建一個token令牌
+
+            // 創建聲明數組
+            var claims = new Claim[]
+           {
+                new Claim(ClaimTypes.Name, "jacky"),
+                new Claim(JwtRegisteredClaimNames.Email, "jacky@gmail.com"),
+                new Claim(JwtRegisteredClaimNames.Sub, "1"),//主題subject，就是id uid
+           };
+
+            // 實例化 token 對象
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("jacky6632003jacky6632003"));//至少16位密鑰
+
+            var token = new JwtSecurityToken(
+                issuer: "http://localhost:5001",
+                audience: "http://localhost:5002",
+                claims: claims,
+                expires: DateTime.Now.AddHours(1),
+                signingCredentials: new SigningCredentials(key, SecurityAlgorithms.HmacSha256)
+            );
+
+            // 生成token
+            var jwtToken = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return new string[] { jwtToken };
+        }
+
+        [HttpGet]
+        [Route("tokens/{jwtStr}")]
+        public ActionResult<IEnumerable<string>> tokens(string jwtStr)
+        {
+            // 獲取token內容的方法
+            //1
+            var jwtHandler = new JwtSecurityTokenHandler();
+            JwtSecurityToken jwtToken = jwtHandler.ReadJwtToken(jwtStr);
+
+            //2
+            var sub = User.FindFirst(d => d.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name")?.Value;
+
+            //3
+            var name = _accessor.HttpContext.User.Identity.Name;
+            var claims = _accessor.HttpContext.User.Claims;
+            var claimTypeVal = (from item in claims
+                                where item.Type == JwtRegisteredClaimNames.Email
+                                select item.Value).ToList();
+
+            return new string[] { JsonConvert.SerializeObject(jwtToken), sub, name, JsonConvert.SerializeObject(claimTypeVal) };
+        }
+    }
+}
---
+++ JWT.Sample/Startup.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JWT.Sample
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
+            var symmetricKeyAsBase64 = "jacky6632003jacky6632003";
+            var keyByteArray = Encoding.ASCII.GetBytes(symmetricKeyAsBase64);
+            var signingKey = new SymmetricSecurityKey(keyByteArray);
+
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = signingKey,
+                ValidateIssuer = true,
+                ValidIssuer = "http://localhost:5001",//發行人
+                ValidateAudience = true,
+                ValidAudience = "http://localhost:5002",//訂閱人
+                ValidateLifetime = true,
+                ClockSkew = TimeSpan.FromSeconds(30),
+                RequireExpirationTime = true,
+            };
+
+            services.AddAuthentication("Bearer")
+             .AddJwtBearer(o =>
+             {
+                 o.TokenValidationParameters = tokenValidationParameters;
+             });
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseAuthentication();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
